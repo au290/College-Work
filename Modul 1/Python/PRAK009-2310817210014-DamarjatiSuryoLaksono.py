5b87291e33ac37f83921b18cbb50b2bafcc70f29
au290 <89144864+au290@users.noreply.github.com>
2023-09-28 18:59:18 +0700

    Add files via upload

---
+++ Modul 1/Python/PRAK009-2310817210014-DamarjatiSuryoLaksono.py
@@ -0,0 +1,9 @@
+
+total_pasukan = 958730
+jumlah_pasukan = 5 
+jumlah_pasukan_per_pahlawan = round(total_pasukan/jumlah_pasukan, 1)
+a="pasukan"
+print("Jumlah pasukan yang dibawa Yu Zhong = ? ")
+print("Jumlah pahlawan = ? ")
+print("Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah ",jumlah_pasukan_per_pahlawan,a)
+  

7b1f4199761a0a0d441027cce5d961e2ef71e97a
au290 <89144864+au290@users.noreply.github.com>
2023-10-02 19:26:58 +0700

    Delete Modul 1 directory

--- Modul 1/Python/PRAK002-2310817210014-DamarjatiSuryoLaksono.py
+++
@@ -1,7 +0,0 @@
-a = 4
-b = 8
-c = 3
-print("Variabel a bernilai", a )
-print("Variabel b bernilai", b )
-print("Variabel c bernilai", c )
-print("Hasil a di kali b di bagi c", round(a*b/c,6))
--- Modul 1/Python/PRAK009-2310817210014-DamarjatiSuryoLaksono.py
+++
@@ -1,9 +0,0 @@
-
-total_pasukan = 958730
-jumlah_pasukan = 5 
-jumlah_pasukan_per_pahlawan = round(total_pasukan/jumlah_pasukan, 1)
-a="pasukan"
-print("Jumlah pasukan yang dibawa Yu Zhong = ? ")
-print("Jumlah pahlawan = ? ")
-print("Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah ",jumlah_pasukan_per_pahlawan,a)
-  

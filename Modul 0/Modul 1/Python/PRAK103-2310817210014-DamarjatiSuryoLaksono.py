6aba019192aac9d3fbfa4bb39f528b10abff5536
au290 <89144864+au290@users.noreply.github.com>
2023-10-02 19:29:33 +0700

    Delete Modul 0/Modul 1 directory

--- Modul 0/Modul 1/Python/PRAK103-2310817210014-DamarjatiSuryoLaksono.py
+++
@@ -1,9 +0,0 @@
-a = 9
-b = 6
-x = 10
-y = 7
-print("Variabel a bernilai", a )
-print("Variabel b bernilai", b )
-print("Variabel x bernilai", x )
-print("Variabel y bernilai", y )
-print("Hasil a di tambah b di kali x di bagi y adalah", round((a+b)*x/y,2))

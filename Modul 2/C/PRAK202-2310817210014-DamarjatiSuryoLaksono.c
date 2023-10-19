d163a6569b0413f8ccb552e4434ba9d49d0a4ad0
au290 <89144864+au290@users.noreply.github.com>
2023-10-19 20:30:04 +0700

    Delete Modul 2 directory

--- Modul 2/C/PRAK202-2310817210014-DamarjatiSuryoLaksono.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  float a, b , i , j , x , y;
-  printf("gas");
-  scanf("%lf", &a, &b , &i , &j , &x , &y);
-  printf("%.3f", a-b*i/j-x+y);
-return 0;
-}
--- Modul 2/Python/PRAK202-2310817210014-DamarjatiSuryoLaksono.py
+++
@@ -1,4 +0,0 @@
-nilai1     = input("Masukkan Nilai Pertama :")
-nilai2     = input("Masukkan Nilai Kedua :")
-hasil      = float(round(float(nilai1)+float(nilai2),2))
-print('Hasil dari penjumlahan nilai pertama "14” dan nilai kedua “20.5” adalah', hasil )

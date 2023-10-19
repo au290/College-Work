0e75555ac79538f09ae177224e32b2b1dd9a773f
au290 <89144864+au290@users.noreply.github.com>
2023-10-19 21:16:54 +0700

    Add files via upload

---
+++ Modul 2/Python/PRAK202-2310817210014-DamarjatiSuryoLaksono.py
@@ -0,0 +1,4 @@
+nilai1     = input("Masukkan Nilai Pertama :")
+nilai2     = input("Masukkan Nilai Kedua :")
+hasil      = float(round(float(nilai1)+float(nilai2),2))
+print(f'Hasil dari penjumlahan nilai pertama "{nilai1}” dan nilai kedua “{nilai2}” adalah  "{hasil}"' )

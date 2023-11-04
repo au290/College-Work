33f9c1a6fc0a14d0364d4511b473f163101bc8b3
au290 <89144864+au290@users.noreply.github.com>
2023-11-04 22:14:24 +0700

    Add files via upload

---
+++ Modul 3/PY/PRAK301-2310817210014-DamarjatiSuryoLaksono.py
@@ -0,0 +1,11 @@
+angka =input("")
+a, b = angka.split()
+
+a = int(a)
+b = int(b)
+
+if (b>a):
+    print(a, b)
+else :
+    print(b, a)
+94
---
+++ Modul 3/PY/PRAK303-2310817210014-DamarjatiSuryoLaksono.py
@@ -0,0 +1,8 @@
+angka = input("")
+angka = int(angka)
+if (angka == 0) :
+    print("Nol")
+elif(angka >0) :
+    print("Positif")
+else : 
+    print("Negatif")
